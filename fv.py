f9ac1f3260310ea9b6fd563fb9fe2461b6e98522
Vadász Dominik <vadaszdominik@EOTVOS2.LOCAL>
2022-01-18 13:11:11 +0100

    bkartya szam gen kész

---
+++ fv.py
@@ -0,0 +1,29 @@
+import os
+import random
+bszam1=[]
+bszam2=[]
+bszam3=[]
+bszam4=[]
+ertek=random.randint(0,9)
+ertek1=random.randint(0,9)
+ertek2=random.randint(0,9)
+ertek3=random.randint(0,9)
+bszam1=(ertek,ertek1,ertek2,ertek3)
+ertek4=random.randint(0,9)
+ertek5=random.randint(0,9)
+ertek6=random.randint(0,9)
+ertek7=random.randint(0,9)
+bszam2=(ertek4,ertek5,ertek6,ertek7)
+
+ertek8=random.randint(0,9)
+ertek9=random.randint(0,9)
+ertek10=random.randint(0,9)
+ertek11=random.randint(0,9)
+bszam3=(ertek8,ertek9,ertek10,ertek11)
+ertek13=random.randint(0,9)
+ertek14=random.randint(0,9)
+ertek15=random.randint(0,9)
+ertek16=random.randint(0,9)
+bszam4=(ertek13,ertek14,ertek15,ertek16)
+
+print(bszam1)
